9ef248c4a01fc5d6274419c73f52f24ba1dde819
mvisovan <155732373+mvisovan@users.noreply.github.com>
2024-05-20 20:29:55 +0300

    Update login_steps.py

--- steps/login_steps.py
+++ steps/login_steps.py
@@ -14,19 +14,19 @@ def step_impl(context):
 #mosteneste din base_page compararea URL si apelez din logimn _page metoda is url correct
 #introducem URL care ma astept sa fie
 
-@when('I write the email adress "mvisovan@gmail.com"')
+@when('I enter "mvisovan@gmail.com" in the email input')
 def step_impl(context):
    context.login_page.set_email("mvisovan@gmail.com")
 
-@when('I write the password  "123456789"')
+@when('I enter "123456" in the password input')
 def step_impl(context):
     context.login_page.set_password("123456789")
 
-@when('I click login')
+@when('I click login button')
 def step_impl(context):
     context.login_page.click_button_register()
 
-@then('I have an question "Ești înregistrat?"')
+@then('I should see the red message "Ești înregistrat?"')
 def step_impl(context):
     assert context.login_page.is_main_error_message_displaid(), "Error mesage not displayed"
     #assert context.login_page.main_error_message_contains_text("Ești înregistrat?"), ("Error message is not contain expected text")
